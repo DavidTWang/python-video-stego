ccc6cba139d8d110244eda44575085f7d572e0d3
Hakus <altenotiz@gmail.com>
2015-11-19 17:59:37 -0800

    iter_frames

--- encode.py
+++ encode.py
@@ -1,10 +1,13 @@
+import subprocess as sp
+import numpy
 import moviepy.editor as mpy
+from pyimage import *
+from pyutils import *
 
 def main():
-    bruce_lee = mpy.VideoFileClip("lee.webm")
-    bruce_lee.save_frame("first_frame.png")
-    print("Done")
-
+    bruce_lee = mpy.VideoFileClip('Samples/lee.webm')
+    for frame in bruce_lee.iter_frames(dtype="uint8"):
+        pass
 
 if __name__ == '__main__':
     main()
